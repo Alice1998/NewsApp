168594a5869e3c9b3b3fa16a32e9690cf3c40684
Alice1998 <alicerdfz@163.com>
2018-09-06 15:17:43 +0800

    NestedListView Fragment1

--- MyApplication9-1/app/src/main/java/com/example/alice/myapplication/FavorFragment.java
+++ MyApplication9-1/app/src/main/java/com/example/alice/myapplication/FavorFragment.java
@@ -1,23 +1,43 @@
 package com.example.alice.myapplication;
-
 import android.graphics.Color;
 import android.os.Bundle;
+
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.ViewFlipper;
 
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.AbsListView.OnScrollListener;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
+import com.example.alice.myapplication.R;
+/**
+ * Created by liang on 2016/8/18.
+ */
 
 public class FavorFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         //加载系统里面的布局文件
         TextView view = (TextView) View.inflate(getActivity(), android.R.layout.simple_list_item_1, null);
-        view.setText("这是收藏页面");
-        view.setBackgroundColor(Color.RED);
+        view.setText("这是设置页面的碎片布局");
+        view.setTextColor(Color.argb(1,6,0,0));
+        view.setBackgroundColor(Color.BLACK);
         return view;
     }
+
 }
--- MyApplication9-1/app/src/main/java/com/example/alice/myapplication/Fragment1.java
+++ MyApplication9-1/app/src/main/java/com/example/alice/myapplication/Fragment1.java
@@ -1,34 +1,58 @@
 package com.example.alice.myapplication;
 
-import android.graphics.Color;
+
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ScrollView;
+import android.widget.ViewFlipper;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class Fragment1 extends Fragment{
 
-    List<LinearLayout> list=new ArrayList<>();
+public class Fragment1 extends Fragment implements AdapterView.OnItemClickListener {
 
+    private NestedListView mListView;
+    private ScrollView mScrollView;
+    private ViewFlipper flipper;
+
+    private ArrayAdapter<String> mAdapter;
 
     @Override
-    public View onCreateView(LayoutInflater inflater,
-                             @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState)
-    {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.fragment1,container,false);
+        mScrollView = (ScrollView) view.findViewById(R.id.scrollview);
+        mListView = (NestedListView) view.findViewById(R.id.listview_1);
+        flipper=(ViewFlipper)view.findViewById(R.id.viewflipper);
+        //启动图片切换
+        flipper.startFlipping();
 
-        TextView view=(TextView)View.inflate(getActivity(),android.R.layout.simple_list_item_single_choice,null);
-        view.setText("this is a fragment for Chinese News");
-        view.setBackgroundColor(Color.GRAY);
+
+        //数据部分
+        List<String> array=new ArrayList<>();
+        for(int i=0;i<20;i++)
+            array.add("This is No. "+i);
+        mAdapter = new ArrayAdapter<>(getActivity(),
+                android.R.layout.simple_list_item_1, array);
+
+        mListView.setAdapter(mAdapter);
+
+        //解决未滑动时聚焦listview的问题
+        mListView.setFocusable(false);
+
         return view;
+    }
 
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
 
     }
+
+
 }
